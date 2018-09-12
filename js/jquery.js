2af5b4ba510e87b8f750ae6a33051d7257cdaf68
guosongjia <guosongjia2017@163.com>
2018-09-12 21:38:50 +0800

    2018.9.12

--- js/jquery.js
+++ js/jquery.js
@@ -3,8 +3,8 @@ function uploadinfo(){
 }
 
 //在Brower按钮后文本框中显示提交序列的文件名
-var file = document.getElementById("myfile")
-var filename = document.getElementById("filename")
-function handleFile(){
-	filename.value = file.value
-}
+//var file = document.getElementById("myfile")
+//var filename = document.getElementById("filename")
+//function handleFile(){
+	//alert("file")
+//}
