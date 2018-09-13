c9adf4cc2a98bca50a24557732c75baa6c69b204
guosongjia <guosongjia2017@163.com>
2018-09-13 10:52:32 +0800

    9.13 1st upload

--- js/jquery.js
+++ js/jquery.js
@@ -8,3 +8,28 @@ function uploadinfo(){
 //function handleFile(){
 	//alert("file")
 //}
+
+
+function check() {
+        
+        var objFile = document.getElementById("myfile");
+        if(objFile.value == "") {
+            alert("Please select your target sequence file.")
+        }
+    
+        console.log(objFile.files[0].size); // 文件字节数
+        
+        var files = $('#myfile').prop('files');//获取到文件列表
+        if(files.length == 0){
+            alert('请选择文件');
+        }else{
+            var reader = new FileReader();//新建一个FileReader
+            reader.readAsText(files[0], "UTF-8");//读取文件 
+            reader.onload = function(evt){ //读取完文件之后会回来这里
+                var fileString = evt.target.result; // 读取文件内容
+                document.getElementById("targetinput").value = fileString
+
+        }
+    }
+    
+}
