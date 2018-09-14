0f5a604155b55ad7e46d7ffe8327fd9bd47c89b0
guosongjia <guosongjia2017@163.com>
2018-09-14 20:34:12 +0800

    2018.9.14 1st commitachieved upload files display

--- js/basicfunctions.js
+++ js/basicfunctions.js
@@ -2,20 +2,6 @@ function uploadinfo(){
 	alert("You have already loaded your target sequence!")
 }
 
-/*$("#uploadbutton").on("click",function(){
-	var fp = $("#myfile");
-	var lg = fp[0].files.length;
-	var items = fp[0].files;
-	var fragment = "";
-
-	if(lg>0){
-		for (var i = 0 ; i<lg;i++){
-			var fileName = items[i].name
-		}
-		fragment += fileName
-	}
-	$("#uploadfile").val()
-})*/
 
 
 
