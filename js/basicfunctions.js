e289edca010bed3f8bdca8114af4d5a1b26ddc8e
guosongjia <guosongjia2017@163.com>
2018-09-15 15:34:03 +0800

    2018.9.15 1st commit with icons and several beauty

--- js/basicfunctions.js
+++ js/basicfunctions.js
@@ -1,18 +1,2 @@
-function uploadinfo(){
-	alert("You have already loaded your target sequence!")
-}
 
-
-
-
-
-function getFileName(obj){
-		 	var fileName="";
-		 	
-			if(typeof(fileName) != "undefined")
-			{
-				fileName = $(obj).val().split("\\").pop();
-				fileName=fileName.substring(0, fileName.lastIndexOf("."));
-			}
-			return fileName;
-	 }
+	
